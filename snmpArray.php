17cb298a1d50b137705f14b468b19ed81a82246b
dcfasika <61594990+dcfasika@users.noreply.github.com>
2020-11-04 20:05:37 -0500

    Update snmpArray.php

--- snmpArray.php
+++ snmpArray.php
@@ -1,19 +1,44 @@
 #!/usr/bin/php
 <?PHP
 
-$data = snmp3_real_walk ('localhost', 'User3', 'authPriv', 'MD5', 'pwd123' , 'DES', 'pwd123', 'TIMETRA-VRTR-MIB::vRtrIfName') ;
+#$data = snmp3_real_walk ('localhost', 'User3', 'authPriv', 'MD5', 'pwd123' , 'DES', 'pwd123', 'TIMETRA-VRTR-MIB::vRtrIfName') ;
+$data = array('[ETRA-VRTR-MIB::vRtrIfName.1.1]' => 'STRING: "intf1"', '[ETRA-VRTR-MIB::vRtrIfName.1.2]' => 'STRING: "intf2"');
 
+echo '<pre>';
 print_r($data);
 
 foreach($data as $key => $val) {
+  $newval = explode(':',trim($val, 'STRING: '));
+  $newkey = explode(' ',trim($key, '[ETRA-VRTR-MIB::vRtrIfName.]'));
+  $data[$key] = $newval;
+  echo '<pre>';
+  print_r($newkey);
+  }
 
-$newval = explode(':',trim($val, 'STRING: '));
-$newkey = explode(' ',trim($key, '[ETRA-VRTR-MIB::IfName.]'));
 echo '<pre>';
-print_r($newkey);
-$data[$key] = $newval;
-
-}
 print_r($data);
 
 ?>
+=========================================================================================================================
+         OUtput
+Array
+(
+    [0] => 1.1
+)
+Array
+(
+    [0] => 1.2
+)
+Array
+(
+    [[ETRA-VRTR-MIB::vRtrIfName.1.1]] => Array
+        (
+            [0] => "intf1"
+        )
+
+    [[ETRA-VRTR-MIB::vRtrIfName.1.2]] => Array
+        (
+            [0] => "intf2"
+        )
+
+)
